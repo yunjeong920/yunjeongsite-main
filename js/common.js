e8e4e60196248af53a7c5e736e12e1714f412c72
yunjeong <yunjeong920@naver.com>
2025-04-08 20:49:42 +0900

    new2

--- js/common.js
+++ js/common.js
@@ -1,24 +1,16 @@
-function scrollHandler() {
+$(function () {
+    gsap.registerPlugin(ScrollTrigger)
 
-  if($(window).scrollTop() >= $(".about").position().top){
-    $(".skill").each(function() {
-      var skill = $(this);
-      var percentage = skill.find(".percentage").text();
-      skill.find(".inner-bar").animate({width: percentage}, 1200);
-    });
-  }
-}
-
-$(window).on("scroll", scrollHandler);
-scrollHandler();
-//섹션 스크롤
-$(".menu-right button").on("click", function() {
-  var id = $(this).attr("id");
-  if (id == "profile-btn") {
-    $("html, body").animate({scrollTop: $(".profile").position().top}, 700)
-  }else if(id == "port-btn") {
-    $("html, body").animate({scrollTop: $(".portfolio").position().top}, 700)
-  }else if (id == "about-btn") {
-    $("html, body").animate({scrollTop: $(".about").position().top}, 700)
-  }
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.visual',
+            start: 'top top',
+            end: "bottom",
+            scrub: 1,
+            pin: true,
+            markers: true,
+        }
+    })
+        .to('.visual h1',{'opacity':'1','ease':'none','dutation':'10'})
+        .to('.visual img',{'scale':'1', 'ease':'none','dutation':'6', 'opacity':'0.8'})
 });
