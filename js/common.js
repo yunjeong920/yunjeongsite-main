392d43d03c04613eae5916fafa09905d4d26bfdb
yunjeong <yunjeong920@naver.com>
2025-04-08 20:59:02 +0900

    new

--- js/common.js
+++ js/common.js
@@ -11,6 +11,6 @@ $(function () {
             markers: false,
         }
     })
-        .to('.visual h1',{'opacity':'1','ease':'none','dutation':'10'})
+        .to('.visual h1',{'opacity':'1','ease':'none','dutation':'25'})
         .to('.visual video',{'scale':'1', 'ease':'none','dutation':'6', 'opacity':'0.8'})
 });
